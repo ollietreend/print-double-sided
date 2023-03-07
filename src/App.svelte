70904c41f3ae0f3ec5a4ca68b6f4c635a223a7ff
Ollie Treend <ollie@treend.uk>
2023-03-07 15:17:40 +0000

    Add basic UI for duplexing PDFs

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,49 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import Duplexer from './lib/Duplexer';
+
+  let files, file;
+  let sideAurl, sideBurl;
+  $: if (files?.length > 0) file = files[0];
+
+  $: if (file) {
+    processFile();
+  }
+
+  const processFile = async () => {
+    const arrayBuffer = await file.arrayBuffer();
+    const duplex = await Duplexer.load(arrayBuffer);
+
+    const sideAblob = new Blob([await duplex.sideA], {
+      type: 'application/pdf',
+    });
+    sideAurl = URL.createObjectURL(sideAblob);
+
+    const sideBblob = new Blob([await duplex.sideB], {
+      type: 'application/pdf',
+    });
+    sideBurl = URL.createObjectURL(sideBblob);
+  };
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <label for="browse-file">Choose a PDF file</label>
+  <input
+    type="file"
+    multiple={false}
+    accept="application/pdf"
+    bind:files
+    id="browse-file"
+  />
+
+  <div class="pdf-output">
+    <iframe src={sideAurl} />
+    <iframe src={sideBurl} />
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .pdf-output {
+    display: flex;
+    height: 80vh;
   }
 </style>
